a9fcac2296517f474239683076da0dc44d50d091
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-02-09 14:38:51 +0530

    remove the last code from middleware file

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,17 +5,6 @@ module.exports = [
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "256mb",
-      jsonLimit: "256mb",
-      textLimit: "256mb",
-      formidable: {
-        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
-    },
-  },
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
