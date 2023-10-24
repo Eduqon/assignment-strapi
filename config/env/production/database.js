73375d92dc87fd8d88371217fabdca9b60cb03ac
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-10-24 18:25:31 +0530

    update the database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,6 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
+console.log({ config });
 module.exports = () => ({
   connection: {
     client: "postgres",
@@ -9,7 +10,7 @@ module.exports = () => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: false,
+      ssl: true,
     },
     debug: false,
   },
